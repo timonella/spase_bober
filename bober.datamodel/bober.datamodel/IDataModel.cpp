8d131574043d338edde37166465721a778a56e14
milists <mihafuntt@gmail.com>
2023-05-29 18:39:00 +0300

    sk

--- bober.datamodel/bober.datamodel/IDataModel.cpp
+++ bober.datamodel/bober.datamodel/IDataModel.cpp
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
 #include "IDataModel.h"
-=======
-<<<<<<< HEAD
-#include "IDataModel.h"
-=======
-#include "IDataModel.h"
->>>>>>> ba81d6403de568a5b14b3ebd49be3f46a1c40b1b
->>>>>>> 562bdd984f62d2f7f95c95a025804145e0d19f0c
