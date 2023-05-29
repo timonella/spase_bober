2e0caca43278b6003c0100334cbc24e2bedc980c
milists <mihafuntt@gmail.com>
2023-05-29 18:32:32 +0300

    skelet

--- bober.datamodel/IDataModel.cpp
+++ bober.datamodel/IDataModel.cpp
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 #include "IDataModel.h"
-=======
-#include "IDataModel.h"
->>>>>>> 562bdd984f62d2f7f95c95a025804145e0d19f0c
+
--- bober.datamodel/IDataModel.h
+++ bober.datamodel/IDataModel.h
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 #pragma once
 class IDataModel
 {
 };
-
-=======
-#pragma once
-class IDataModel
-{
-};
-
->>>>>>> 562bdd984f62d2f7f95c95a025804145e0d19f0c
