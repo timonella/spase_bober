c6c1311fa1a117e3324480f491c675039157ddef
forttort <128396656+forttort@users.noreply.github.com>
2023-05-31 16:05:43 +0300

    Добавил описание

--- Tamer universe/GameEngine.cpp
+++ Tamer universe/GameEngine.cpp
@@ -2,8 +2,11 @@
 
  
 
-
+//запрос команд
 void GameEngine::get_comands() {}
+//отображение меню сохранения/загрузки
 void GameEngine::show_save_load_options() {}
+//бой кораблей и захват планет
 void GameEngine::battle_and_capture(){}
+//отображение инофмации о имперерии
 void GameEngine::show_empire_info(){}
