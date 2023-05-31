7e9441cedcac5e608496d088e43b37d884afb2fa
forttort <128396656+forttort@users.noreply.github.com>
2023-05-31 16:20:12 +0300

    описание

--- Tamer universe/IGameEngine.h
+++ Tamer universe/IGameEngine.h
@@ -5,7 +5,7 @@ public:
 	//запрос команд
 	virtual void get_comands() {};
 	//отображение меню сохранения/загрузки
-	virtual void show_save_load_options() {}; 
+	virtual void show_save_load_options() {};
 	//бой кораблей и захват планет
 	virtual void battle_and_capture() {};
 	//показ информации о империи
