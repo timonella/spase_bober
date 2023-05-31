98e62a52119a46d5d35c2f6b7b49cd82cf09a3fd
forttort <128396656+forttort@users.noreply.github.com>
2023-05-31 16:34:02 +0300

    добавил команды

--- Tamer universe/IGameEngine.h
+++ Tamer universe/IGameEngine.h
@@ -1,8 +1,14 @@
 #pragma once
 class IGameEngine {
 public:
+	//запрос команд
+	virtual void get_comands() {};
+	//отображение меню сохранения/загрузки
+	virtual void show_save_load_options() {};
+	//бой кораблей и захват планет
+	virtual void battle_and_capture() {};
+	//показ информации о империи
+	virtual void show_empire_info() {};
 
-	//rename
-	/*virtual void InputInfo() {};
-	virtual void CoutInfoLoad() {};*/
+
 };
