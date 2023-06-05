5278c24bfd03a267499bd33d7d3245dd77159f1d
RespectAssasin <makarovs220@gmail.com>
2023-06-05 15:30:05 +0300

    add Definition for Load()

--- Tamer universe/Tamer universe/SaveLoadEngine.cpp
+++ Tamer universe/Tamer universe/SaveLoadEngine.cpp
@@ -1,4 +1,27 @@
+#include <iostream>
 #include "SaveLoadEngine.h"
+#include "pugixml.hpp"
 
-void SaveLoadEngine::Save() {}
-void SaveLoadEngine::Load() {}
+using namespace std;
+using namespace pugi;
+
+void SaveLoadEngine::Save() {
+	
+}
+void SaveLoadEngine::Load() {
+    xml_document doc;
+
+    /*if (!doc.load_file("sample.xml")) return -1;*/
+    xml_node tools = doc.child("TamerUniverse").child("Planets");
+
+    for (xml_node_iterator it = tools.begin(); it != tools.end(); ++it) {
+        cout << "Planet:";
+        for (xml_attribute_iterator ait = it->attributes_begin();
+            ait != it->attributes_end(); ++ait) {
+            cout << " " << ait->name() <<
+                " = " << ait->value();
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
