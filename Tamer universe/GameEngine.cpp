4c18acf219d455c8cf6bc3b43d03c25646b89a14
forttort <128396656+forttort@users.noreply.github.com>
2023-06-05 16:27:02 +0300

    Update GameEngine.cpp

--- Tamer universe/GameEngine.cpp
+++ Tamer universe/GameEngine.cpp
@@ -1,10 +1,26 @@
 #include "GameEngine.h"
+#include <iosstream>
+
+using namespace std;
 
  
 //запрос команд
 void GameEngine::GetCommands() {}
 //отображение меню сохранения/загрузки
-void GameEngine::ShowSaveLoadMenu() {}
+void GameEngine::ShowSaveLoadMenu() {
+	cout << "+--------------+";
+	cout << "|Tamer Universe|";
+	cout << "+--------------+";
+	cout << endl;
+	cout << "+--------------------+";
+	cout << "|        MENU		  |";
+	cout << "|   Tamer Universe	  |";
+	cout << "|1-Load		2-Save|";
+	cout << "+--------------------+";
+	cout << endl;
+
+
+}
 //бой кораблей и захват планет
 void GameEngine::BattleAndCapture() {}
 //отображение инофмации о имперерии
