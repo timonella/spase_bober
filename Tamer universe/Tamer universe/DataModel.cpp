6a415cffe2fb29baf8dbf761ee71a99dfeba3ee6
milists <mihafuntt@gmail.com>
2023-06-05 18:21:00 +0300

    th

--- Tamer universe/Tamer universe/DataModel.cpp
+++ Tamer universe/Tamer universe/DataModel.cpp
@@ -4,18 +4,8 @@
 #include <iostream>
 using namespace std;
 void Planet() {
-	srand(time(NULL));
-	const int n = 10;
-	double planet[n];
-	for (int i = 0; i < n; i++) {
-		planet[i] = (rand() % n) + 1;
-	}
+
 }
 void Fleet() {
-	srand(time(NULL));
-	const int n = 10;
-	double Fleet[n];
-	for (int i = 0; i < n; i++) {
-		Fleet[i] = (rand() % n) + 1;
-	}
+
 }
