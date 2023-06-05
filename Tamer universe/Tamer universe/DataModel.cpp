00431dad5742530a2384cf44b41c430b4bab505c
milists <mihafuntt@gmail.com>
2023-06-05 18:19:21 +0300

    jhg

--- Tamer universe/Tamer universe/DataModel.cpp
+++ Tamer universe/Tamer universe/DataModel.cpp
@@ -1,1 +1,21 @@
 #include "DataModel.h"
+#include<random>
+#include <time.h>
+#include <iostream>
+using namespace std;
+void Planet() {
+	srand(time(NULL));
+	const int n = 10;
+	double planet[n];
+	for (int i = 0; i < n; i++) {
+		planet[i] = (rand() % n) + 1;
+	}
+}
+void Fleet() {
+	srand(time(NULL));
+	const int n = 10;
+	double Fleet[n];
+	for (int i = 0; i < n; i++) {
+		Fleet[i] = (rand() % n) + 1;
+	}
+}
