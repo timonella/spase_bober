e779b30529bd5a641a6ec9cd9eac02542a8f8440
DrShadowich <129182414+DrShadowich@users.noreply.github.com>
2023-06-05 16:57:41 +0300

    Delete IModelGenerator.h

--- Tamer universe/Tamer universe/IModelGenerator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class IModelGenerator{
-public:
-	virtual void loadmap() {};
-	virtual void generateplanet() {};
-	virtual void planetsforplayers() {};
-	virtual void toDatamodel() {};
-};
