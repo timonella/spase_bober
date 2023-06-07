aaeb779a626bf6f69c6cb234dfb82217e51be0b4
forttort <128396656+forttort@users.noreply.github.com>
2023-06-07 16:30:53 +0300

    добавил отрисовку меню

--- Tamer universe/Tamer universe/GameEngine.cpp
+++ Tamer universe/Tamer universe/GameEngine.cpp
@@ -1,13 +1,28 @@
 #include "GameEngine.h"
-
+#include <iostream>
+using namespace std;
  
 
 //запрос команд
 void GameEngine::GetComands() {}
 //отображение меню загрузки
-void GameEngine::ShowLoadOptions() {}
+void GameEngine::ShowLoadOptions() {
+	cout << "+--------------+";
+	cout << "|Tamer Universe|";
+	cout << "+--------------+";
+	cout << "+--------------+";
+	cout << "|     Menu     |";
+	cout << "|    1-Load    |";
+	cout << "+--------------+";
+
+}
 //отображение меню сохранения/загрузки
-void GameEngine::ShowSaveLoadOptions() {}
+void GameEngine::ShowSaveLoadOptions() {
+	cout << "+---------------+";
+	cout << "|      Menu     |";
+	cout << "| 1-Load 2-Save |";
+	cout << "+---------------+";
+}
 //бой кораблей и захват планет
 void GameEngine::BattleAndCapture(){}
 //отображение инофмации о имперерии
