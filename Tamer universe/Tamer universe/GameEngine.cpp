43a9e03da35106ea5ac4dbb74ff04a24a1e9ee41
forttort <128396656+forttort@users.noreply.github.com>
2023-06-07 16:49:55 +0300

    теперь у нас есть кораблик ураааааааа

--- Tamer universe/Tamer universe/GameEngine.cpp
+++ Tamer universe/Tamer universe/GameEngine.cpp
@@ -7,21 +7,39 @@ using namespace std;
 void GameEngine::GetComands() {}
 //отображение меню загрузки
 void GameEngine::ShowLoadOptions() {
-	cout << "+--------------+";
-	cout << "|Tamer Universe|";
-	cout << "+--------------+";
-	cout << "+--------------+";
-	cout << "|     Menu     |";
-	cout << "|    1-Load    |";
-	cout << "+--------------+";
+	cout << "                     `. ___" <<  endl;
+	cout << "                    __,' __`.                _..----....____" << endl;
+	cout << "        __...--.'``;.   ,.   ;``--..__     .'    ,-._    _.-'" << endl;
+	cout << "  _..-''-------'   `'   `'   `'     O ``-''._   (,;') _,'" << endl;
+	cout << ",'________________                          \\`-._`-','" << endl;
+	cout << " `._              ```````````------...___   '-.._'-:" << endl;
+	cout << "    ```--.._      ,.                     ````--...__\\-." << endl;
+	cout << "            `.--. `-`                       ____    |  |`" << endl;
+	cout << "              `. `.                       ,'`````.  ;  ;`" << endl;
+	cout << "                `._`.        __________   `.      \\'__/`" << endl;
+	cout << "                   `-:._____/______/___/____`.     \\  `" << endl;
+	cout << "                               |       `._    `.    \\" << endl;
+	cout << "                               `._________`-.   `.   `.___" << endl;
+	cout << "                                             SSt  `------'`" << endl;
+	cout << "" << endl;
+
+
+
+	cout << "+--------------+" << endl;
+	cout << "|Tamer Universe|" << endl;
+	cout << "+--------------+" << endl;
+	cout << "+--------------+" << endl;
+	cout << "|     Menu     |" << endl;
+	cout << "|    1-Load    |" << endl;
+	cout << "+--------------+"<<endl;
 
 }
 //отображение меню сохранения/загрузки
 void GameEngine::ShowSaveLoadOptions() {
-	cout << "+---------------+";
-	cout << "|      Menu     |";
-	cout << "| 1-Load 2-Save |";
-	cout << "+---------------+";
+	cout << "+---------------+"<<endl;
+	cout << "|      Menu     |"<<endl;
+	cout << "| 1-Load 2-Save |"<<endl;
+	cout << "+---------------+"<<endl;
 }
 //бой кораблей и захват планет
 void GameEngine::BattleAndCapture(){}
