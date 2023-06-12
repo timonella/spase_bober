1443240a4e87f01a24e8f9f4b3d6f41db4cbccb2
RespectAssasin <makarovs220@gmail.com>
2023-06-13 02:32:53 +0300

    Added more clear comments.

--- Tamer universe/Tamer universe/SaveLoadEngine.cpp
+++ Tamer universe/Tamer universe/SaveLoadEngine.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include "SaveLoadEngine.h"
-
 using namespace std;
+string filepath = "Universe.xml";
 
 void SaveLoadEngine::CreateXML() {
-	ofstream file;
-	file.open("Universe.xml");
+	ofstream file(filepath);
+
 	if (file.is_open()) {
-		file << "<Universe>\n\t<Turn>FirstPlayer</Turn>\n\n\t<Planets>\n\t\t<Planet>\n\t\t\t<Fleet>\n\t\t\t\t<Ship></Ship>\n\t\t\t</Fleet>\n\t\t\t<Owner>FiratPlayer</Owner>\n\t\t\t<ID>Jupiter</ID>\n\t\t</Planet>\n\t</Planets>\n</Universe>";
+		file << "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<Universe>\n\t<Turn>FirstPlayer</Turn>\n\n\t<Planets>\n\t\t<Planet>\n\t\t\t<Fleet>\n\t\t\t\t<Ship>2</Ship>\n\t\t\t</Fleet>\n\t\t\t<Owner>FirstPlayer</Owner>\n\t\t\t<ID>Jupiter</ID>\n\t\t</Planet>\n\n\t\t<Planet>\n\t\t\t<Fleet>\n\t\t\t\t<Ship>2</Ship>\n\t\t\t</Fleet>\n\t\t\t<Owner>SecondPlayer</Owner>\n\t\t\t<ID>Mars</ID>\n\t\t</Planet></Planets>\n</Universe>";
+		file.close();
 	}
 }
-void SaveLoadEngine::Save() {}
-void SaveLoadEngine::Load() {}
+	void SaveLoadEngine::Save() {}
+	void SaveLoadEngine::Load() {}
