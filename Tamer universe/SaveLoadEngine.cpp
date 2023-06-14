47db614e0db6e16e3e53551405015b3e35e13c6d
RespectAssasin <makarovs220@gmail.com>
2023-06-14 18:12:36 +0300

    delete </Planet>

--- Tamer universe/SaveLoadEngine.cpp
+++ Tamer universe/SaveLoadEngine.cpp
@@ -8,7 +8,7 @@ void SaveLoadEngine::CreateXML() {
 	ofstream file(filepath);
 
 	if (file.is_open()) {
-		file << "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<Universe>\n\t<Turn>FirstPlayer</Turn>\n\n\t<Planets>\n\t\t<Planet>\n\t\t\t<Fleet>\n\t\t\t\t<Ship>2</Ship>\n\t\t\t</Fleet>\n\t\t\t<Owner>FirstPlayer</Owner>\n\t\t\t<ID>Jupiter</ID>\n\t\t</Planet>\n\n\t\t<Planet>\n\t\t\t<Fleet>\n\t\t\t\t<Ship>2</Ship>\n\t\t\t</Fleet>\n\t\t\t<Owner>SecondPlayer</Owner>\n\t\t\t<ID>Mars</ID>\n\t\t</Planet></Planets>\n</Universe>";
+		file << "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<Universe>\n\t<Turn>FirstPlayer</Turn>\n\n\t<Planets>\n\t\t<Planet>\n\t\t\t<Fleet>\n\t\t\t\t<Ship>2</Ship>\n\t\t\t</Fleet>\n\t\t\t<Owner>FirstPlayer</Owner>\n\t\t\t<ID>Jupiter</ID>\n\t\t</Planet>\n\n\t\t<Planet>\n\t\t\t<Fleet>\n\t\t\t\t<Ship>2</Ship>\n\t\t\t</Fleet>\n\t\t\t<Owner>SecondPlayer</Owner>\n\t\t\t<ID>Mars</ID>\n\t\t</Planet>\n</Universe>";
 		file.close();
 	}
 }
