8af12762a95e747ecb504f5cfa7d3d0c9d9eca06
RespectAssasin <makarovs220@gmail.com>
2023-06-14 18:54:47 +0300

    add enum 2.0

--- Tamer universe/Source.cpp
+++ Tamer universe/Source.cpp
@@ -3,12 +3,19 @@
 using namespace std;
 
 int main() {
-	
+	srand(time(NULL));
 	enum Bobers { apples, pear, balls };
 
 	Bobers arr[20];
+	int aple = 0;
 	for (int i = 0; i < 20, i++) {
 		arr[i] = (Bobers)(rand() % 3);
+	}
+	for (int i = 0; i < 20; i++) {
+		if (arr[i] == Bobers::apples) {
+			aple++;
+		}
+		else if (arr[i] == Bobers::pear);
 	}
 
 	return 0;
