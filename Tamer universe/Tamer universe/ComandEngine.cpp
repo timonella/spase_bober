944d4b925f121c0a7d296a4e33a7156dd7b38b1e
RespectAssasin <makarovs220@gmail.com>
2023-06-14 17:10:14 +0300

    delete changes in .cpp

--- Tamer universe/Tamer universe/ComandEngine.cpp
+++ Tamer universe/Tamer universe/ComandEngine.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 void ComandEngine::FinishTurn() {}
+
 void ComandEngine::ShowUniverseStatus() {}
+
 void ComandEngine::SendFleet(int targetPlanetId) {}
+
 void ComandEngine::BuildShip(int attack, int shield) {}
+
 void ComandEngine::Start() {}
--- Tamer universe/Tamer universe/DataModel.cpp
+++ Tamer universe/Tamer universe/DataModel.cpp
@@ -1,9 +1,12 @@
 #include "DataModel.h"
-
 using namespace std;
+
 void DataModel::SendFleet(int planetIndex){}
+
 void DataModel::BuildShip(int shield, int attack, int planetIndex){}
+
 void DataModel::StartTurn(){}
+
 void DataModel::EndTurn(){}
 
 
