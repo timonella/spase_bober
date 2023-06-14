aa59f69089f427bb3b1b5ff00669d0b0c2846842
RespectAssasin <makarovs220@gmail.com>
2023-06-14 18:51:09 +0300

    add enum

--- Tamer universe/Source.cpp
+++ Tamer universe/Source.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 int main() {
 	
-	SaveLoadEngine file;
-	file.CreateXML();//ѕока что просто пример создани€ xml-файла с тестовыми данными игроков, при условии, что изначально он не включен в проект и не существует.
+	enum Bobers { apples, pear, balls };
+
+	Bobers arr[20];
+	for (int i = 0; i < 20, i++) {
+		arr[i] = (Bobers)(rand() % 3);
+	}
 
 	return 0;
 }
