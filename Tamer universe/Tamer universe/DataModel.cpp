b90febeb2c471ebe23c55c46ba8ad650a3c6e385
milists <mihafuntt@gmail.com>
2023-06-21 18:14:02 +0300

    frt

--- Tamer universe/Tamer universe/DataModel.cpp
+++ Tamer universe/Tamer universe/DataModel.cpp
@@ -1,17 +1,14 @@
 #include "DataModel.h"
-
 #include "IDataModel.h"
 #include <iostream>
-
 using namespace std;
 void DataModel::SendFleet(int planetIndex){}
-
 void DataModel::BuildShip(int shield, int attack, int planetIndex){
 	
 }
-
 void DataModel::StartTurn(){
 	
 }
+void DataModel::EndTurn(){
 
-void DataModel::EndTurn(){}
+}
