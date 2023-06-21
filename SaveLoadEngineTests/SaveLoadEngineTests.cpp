e349f5fc7dda1720c39e81fefb28ae06360493ac
RespectAssasin <makarovs220@gmail.com>
2023-06-21 18:51:49 +0300

    add realization

---
+++ SaveLoadEngineTests/SaveLoadEngineTests.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SaveLoadEngineTests
+{
+	TEST_CLASS(TaskTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1) {
+			int a = 100;
+			int b = 200;
+
+			Assert::AreEqual(300, a + b);
+		}
+	};
+}
--- Tamer universe/XMLBuilder.cpp
+++ Tamer universe/XMLBuilder.cpp
@@ -1,31 +1,31 @@
 #include "XMLBuilder.h"
 #include <fstream>
+using namespace std;
 
 char* WriteXML::WriteHeader() {
-
+	return (char*) "<?xml version=\"1.0\" encoding=\"utf - 8\"?>\r\n";
 }
 char* WriteXML::WriteUniverse(char* header) {
-	ofstream file("Universe.xml");
-
+	return (char*) _CONCAT("<Universe>", "</Universe>");
 }
 char* WriteXML::WriteTurn() {
-
+	return (char*)
 }
 char* WriteXML::WritePlanets() {
-
+	return (char*)
 }
 char* WriteXML::WritePlanet() {
-
+	return (char*)
 }
 char* WriteXML::WriteFleet() {
-
+	return (char*)
 }
 char* WriteXML::WriteShip() {
-
+	return (char*)
 }
 char* WriteXML::WriteOwner() {
-
+	return (char*)
 }
 char* WriteXML::WriteID() {
-
+	return (char*)
 }
