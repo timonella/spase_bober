0aeaad6243e44a8f36327c935921c1e64d06d13f
milists <mihafuntt@gmail.com>
2023-06-21 18:24:56 +0300

    erdh

--- Tamer universe/Tamer universe/DataModel.cpp
+++ Tamer universe/Tamer universe/DataModel.cpp
@@ -2,7 +2,9 @@
 #include "IDataModel.h"
 #include <iostream>
 using namespace std;
-void DataModel::SendFleet(int planetIndex){}
+void DataModel::SendFleet(int planetIndex){
+
+}
 void DataModel::BuildShip(int shield, int attack, int planetIndex){
 	
 }
