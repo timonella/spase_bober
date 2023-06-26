82d070b7d6ef225ae7cb3a4e36d90412f002db4d
forttort <>
2023-06-26 16:41:25 +0300

    rt

--- Tamer universe/Tamer universe/DataModel.cpp
+++ Tamer universe/Tamer universe/DataModel.cpp
@@ -1,19 +1,16 @@
 #include "DataModel.h"
-<<<<<<< HEAD
 #include "IDataModel.h"
 #include <iostream>
-=======
-
->>>>>>> main
 using namespace std;
-void DataModel::SendFleet(int planetIndex){}
+void DataModel::SendFleet(int planetIndex){
 
+}
 void DataModel::BuildShip(int shield, int attack, int planetIndex){
 	
 }
-
 void DataModel::StartTurn(){
 	
 }
+void DataModel::EndTurn(){
 
-void DataModel::EndTurn(){}
+}
---
+++ Tamer universe/Tamer universe/comandEngie Test/comandEngie Test.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace comandEngieTest
+{
+	TEST_CLASS(comandEngieTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a = 1;
+		}
+
+		TEST_METHOD(TestMethod2) {
+
+			int b = 10;
+			int bober = 10;
+			Assert::AreEqual(10, b - bober);
+		}
+		TEST_METHOD(TestMethod3) {
+
+			int bober[5];
+			for (int i = 0; i < 5; i++) {
+				bober[i] = i;
+			}
+
+			Assert::AreEqual(1, bober[0]+bober[1]);
+
+		}
+
+	};
+}
